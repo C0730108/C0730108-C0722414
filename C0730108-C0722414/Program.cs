a21a0edd2b348b983949041e218055c7de2b46c2
jashandeepkaur <jashandeepkaur717@gmail.com>
2019-03-14 13:50:59 -0400

    commit tag

--- C0730108-C0722414/Program.cs
+++ C0730108-C0722414/Program.cs
@@ -8,8 +8,83 @@ namespace C0730108_C0722414
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-        }
+
     }
-}
+        
+            public class BankAccount
+        {
+            private string m_customerName;
+
+            private double m_balance;
+            private bool m_frozon = false;
+            private BankAccount()
+            {
+            }
+            public BankAccount(string customerName,double balance)
+            {
+                m_customerName = customerName;
+                m_balance = balance;
+
+            }
+            public string CustomerName
+            {
+                get { return m_customerName; }
+            }
+            public double Balance
+            {
+                get
+                {
+                    return m_balance;
+                }
+            }
+            public void Debit(double amount)
+            {
+                if (m_frozon)
+                {
+                    throw new Exception("Account frozon");
+
+                }
+                if (amount > m_balance)
+                {
+                    throw new ArgumentOutOfRangeException("amount");
+                }
+                if (amount < 0)
+                {
+                    throw new ArgumentOutOfRangeException("amount");
+                }
+                m_balance += amount;
+            }
+        public void Credit(double amount)
+        {
+            if (m_frozon)
+            {
+                throw new Exception("Account frozon");
+
+            }
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            m_balance += amount;
+        }
+                private void FreezeAccount()
+                {
+                    m_frozon = true;
+
+                }
+                private void UnFreezeAccount()
+                {
+                    m_frozon = false;
+                }
+                public static void Main()
+                {
+                    BankAccount ba = new BankAccount("Mr.Bryan Walton", 11.99);
+                    ba.Credit(5.77);
+                    ba.Debit(11.99);
+                    Console.WriteLine("Current balance is ${0}", ba.Balance);
+                }
+            }
+  }
+        
+    
+
