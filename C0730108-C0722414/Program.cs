1c6e085e1e40d36cf7deac673d0d229d35984713
jashandeepkaur <jashandeepkaur717@gmail.com>
2019-03-14 14:13:36 -0400

    COMMIT

--- C0730108-C0722414/Program.cs
+++ C0730108-C0722414/Program.cs
@@ -81,7 +81,7 @@ namespace C0730108_C0722414
                     BankAccount ba = new BankAccount("Mr.Bryan Walton", 11.99);
                     ba.Credit(5.77);
                     ba.Debit(11.22);  
-                    Console.WriteLine("Current balance is ${0}", ba.Balance);
+                    Console.WriteLine("Current balance is ${0}", ba.Balance);  
                 }
             }
   }
